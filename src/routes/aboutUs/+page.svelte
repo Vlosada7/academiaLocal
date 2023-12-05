30f0cde568081433ccaaf47fecd3a91911efd182
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-12-06 00:56:30 +0100

    faq component design

--- src/routes/aboutUs/+page.svelte
+++ src/routes/aboutUs/+page.svelte
@@ -1,5 +1,6 @@
 <script>
   import Intro from "$lib/Intro.svelte"
+	import Info from "../../lib/Info.svelte";
 </script>
 
 <Intro 
@@ -10,3 +11,18 @@
   btn = 'Modalities'
   href = '/modalities'
 />
+<section class="faq">
+  <div class="faq__wrapper wrap">
+    <Info 
+      question = "How much is the monthly fee?"
+      answer = "The monthly fee will vary between 40 and 100 euros, depending on the number of days the student will attend the gym during the week."
+    />
+  </div>
+</section>
+
+
+<style>
+  .faq {
+    margin-top: 60px;
+  }
+</style>
