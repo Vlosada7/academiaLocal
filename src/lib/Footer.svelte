0d1ff58c340dd6ae57100552f8ab9ec32743074c
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-12-06 00:33:25 +0100

    footer align at end

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -16,11 +16,6 @@
       </div>
     </div>
   </div>
-  <div class="credits">
-    <div class="credits__wrapper wrap">
-      <span>© Local Gym, 2023</span>
-    </div>
-  </div>
 </footer>
 
 <style>
@@ -57,20 +52,5 @@
     display: flex;
     gap: 8px; 
   }
- .credits {
-  margin-top: 60px;
- }
- .credits__wrapper::before {
-  content: '';
-  display: block;
-  width: 100%;
-  height: 1px;
-  background-color: var(--subtle);
- }
- span {
-  display: inline-block;
-  font-size: calc(14 / 16 * 1rem);
-  padding-top: 24px;
-  padding-bottom: 24px;
- }
+
 </style>
