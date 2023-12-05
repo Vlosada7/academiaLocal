446c7cc0ff6248f997d0bd1efd5d29a7bb1765e2
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-12-05 20:45:16 +0100

    first section from homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<h2>Victor Losada</h2>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import imageHome from '../assets/musculacao.jpg'
+</script>
 
 
+<section class="intro" style="background-image: url({imageHome});">
+  <div class="intro__wrapper wrap">
+    <h1>Are you ready for the challenge?</h1>
+    <p>The Local Gym is ready to help you achieve your goals.</p>
+    <a href="/sobre">About us</a>
+  </div>
+</section>
+
+<style>
+  .intro{
+    min-height: 360px;
+    background-size: cover;
+    background-position: center;
+    display: grid;
+    align-content: center;
+    justify-content: start;
+    position: relative;
+  }
+  .intro::after {
+    content:'';
+    position: absolute;
+    inset: 0;
+    background-color: rgba(0, 0, 0, 0.55);
+  }
+  .intro__wrapper {
+    z-index: 1;
+  }
+</style>
