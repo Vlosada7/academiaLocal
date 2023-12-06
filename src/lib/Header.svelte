50f7ba362cbd70639485a56913623d9d3add5401
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-12-06 16:47:55 +0100

    opt actual page on menu

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,5 +1,8 @@
 <script>
   import Logo from "$lib/assets/logo.png"
+  import { page } from '$app/stores'
+
+  $: current = $page.route.id;
 </script>
 
 <header class="header">
@@ -7,9 +10,9 @@
     <a href="/" class="logo"><img src="{Logo}" alt="Local Gym Logo"></a>
     <nav>
       <ul>
-        <li><a href='/'>Home</a></li>
-        <li><a href='/aboutUs'>About Us</a></li>
-        <li><a href='/modalities'>Modalities</a></li>
+        <li><a href='/' class:active={current === '/'}>Home</a></li>
+        <li><a href='/aboutUs' class:active={current === '/aboutUs'}>About Us</a></li>
+        <li><a href='/modalities' class:active={current === '/modalities' || current === '/modalities/[slug]'}>Modalities</a></li>
         <li><a class="btn header__btn" href="#contact">Contact Us</a></li>
       </ul>
     </nav>
@@ -49,6 +52,11 @@
 
   a:not(.btn):hover {
     text-decoration: underline;
+  }
+  a.active,
+  a.active:hover {
+    text-decoration: underline;
+    text-decoration-color: var(--feat);
   }
 
   @media (max-width: 800px) {
