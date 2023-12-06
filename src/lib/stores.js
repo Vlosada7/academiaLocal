a3202efc9310cc7e2c2b473916e71386f766726f
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-12-06 13:00:14 +0100

    single page

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -5,25 +5,34 @@ export const modalities = writable([
 		name: "Weight Training",
 		slug: "weightTrain",
 		img: "/musculacao.jpg",
+		content: "Bodybuilding workouts guided by our experts",
 	},
 	{
 		name: "Crossfit",
 		slug: "crossfit",
 		img: "/crossfit.jpg",
+		content:
+			"Group or individual functional training classes, with HIT and fully dynamic, guided by our highly qualified athletes.",
 	},
-  {
-    name: 'Stretching',
-    slug: 'stretching',
-    img: '/alongamento.jpg'
-  },
-  {
-    name: 'Group Exercise',
-    slug: 'groupExe',
-    img: '/exercicios.jpg'
-  },
-  {
-    name: 'Swimming',
-    slug: 'swimming',
-    img: '/natacao.jpg'
-  }
+	{
+		name: "Stretching",
+		slug: "stretching",
+		img: "/alongamento.jpg",
+		content:
+			"Group training, ideal for loosening muscles and improving performance both in sports and in everyday life.",
+	},
+	{
+		name: "Group Exercise",
+		slug: "groupExe",
+		img: "/exercicios.jpg",
+		content:
+			"Guided exercise class aimed at improving your performance and stamina.",
+	},
+	{
+		name: "Swimming",
+		slug: "swimming",
+		img: "/natacao.jpg",
+		content:
+			"Swimming lessons for all ages, whether to achieve your goals in the lane or to learn how to swim.",
+	},
 ]);
---
+++ src/routes/modalities/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { page } from '$app/stores'
+  import { modalities } from '$lib/stores'
+
+  let modalitie = $page.params.slug
+  
+  let data = $modalities.find(e => e.slug === modalitie);
+  console.log(data);
+</script>
+
+<h1>{data.name}</h1>
+
