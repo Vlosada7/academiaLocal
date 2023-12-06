c0257d88b4110f874a62204dc3bf3da6df933a86
Victor Losada <117365425+Vlosada7@users.noreply.github.com>
2023-12-06 15:42:53 +0100

    remove actual page from other modalities

--- src/routes/modalities/[slug]/+page.svelte
+++ src/routes/modalities/[slug]/+page.svelte
@@ -3,10 +3,8 @@
   import { modalities } from '$lib/stores'
 	import IntroSingle from '$lib/IntroSingle.svelte';
 	import Card from '../../../lib/Card.svelte';
+  import autoAnimate from "@formkit/auto-animate"
 
-  let modalitie = $page.params.slug
-  
-  // let data = $modalities.find(e => e.slug === modalitie);
   export let data;
 </script>
 
@@ -15,9 +13,11 @@
 <section class="related">
   <div class="related__wrapper wrap">
     <h3>Other modalities:</h3>
-    <div class="list">
+    <div class="list" use:autoAnimate>
       {#each $modalities as info}
+      {#if info.slug !== $page.params.slug}
       <Card info={info}/>
+      {/if}
       {/each}
     </div>
   </div>
